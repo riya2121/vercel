f1809ae52fde12a22aa1aeff70dbabd62a312263
riya2121 <36795880+riya2121@users.noreply.github.com>
2023-04-11 20:48:12 +0530

    Add files via upload

---
+++ your_action.php
@@ -0,0 +1,26 @@
+if (isset($_POST['g-recaptcha-response'])) {
+    $captcha = $_POST['g-recaptcha-response'];
+} else {
+    $captcha = false;
+}
+
+if (!$captcha) {
+    //Do something with error
+} else {
+    $secret   = '6LdL6nklAAAAAB94Z7Phet-xecPqFMFBiza1TTo1;
+    $response = file_get_contents(
+        "https://www.google.com/recaptcha/api/siteverify?secret=" . $secret . "&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR']
+    );
+    // use json_decode to extract json response
+    $response = json_decode($response);
+
+    if ($response->success === false) {
+        //Do something with error
+    }
+}
+
+//... The Captcha is valid you can continue with the rest of your code
+//... Add code to filter access using $response . score
+if ($response->success==true && $response->score <= 0.5) {
+    console.log("Hello");
+}
